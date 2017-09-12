aa7bbce985bcd210e3f56c79f3c6cbcf026ccf6a
cl <caolei@gznb.com>
2017-09-12 10:26:55 +0800

    Redis配置

---
+++ src/main/java/com/flhscl/springbootscrafhold/config/cache/RedisConfig.java
@@ -0,0 +1,11 @@
+package com.flhscl.springbootscrafhold.config.cache;
+
+/**
+ * @author <a href="mailto:caolei@gznb.com">cl</a>
+ * @version 1.0 & 2017/8/30
+ * @since 1.0
+ * To change this template use File | Settings | Editor | File and Code Templates
+ * Description:
+ */
+public class RedisConfig {
+}
